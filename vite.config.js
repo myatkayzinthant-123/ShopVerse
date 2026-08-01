c6917f45b82e3635ec71b926fa118b02fa762776
myatkayzinthant-123 <kkzt31583@gmail.com>
2026-08-01 22:11:31 +0630

    fix routing issue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const base = '/ShopVerse'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  base
 })
